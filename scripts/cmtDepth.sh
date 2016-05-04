7c0c4be723e94b97be724a84a33591e07ffdcea1
njlindsey <nathaniel.j.lindsey@gmail.com>
2016-05-04 05:32:44 -0700

    get plotting on xy axis

--- scripts/cmtDepth.sh
+++ scripts/cmtDepth.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # MOMENT TENSOR DEPTH ANALYSIS SCRIPT
 # NATE LINDSEY April 2016
-outfile=temp.out
 
-# TITLE
-echo "CMT Inversion Depth Analysis for Alberta Fox Creek M4.2" > $outfile
+# SETUP FILES
+outfile=cmtDepth.out
+temp=cmtDepth.temp
+plot=cmtDepth.ps
+
+# CLEAR FILES
+> $outfile
+> $temp
+> $plot
 
 # COPY inversion input to scratch
 cp mt_inv.in mt_inv_saved.in
@@ -17,12 +23,24 @@ for i in {1..25}; do
 	sed -i .bup "7 s/_241d.* /_241d$i /" mt_inv.in
 	sed -i .bup "8 s/_258d.* /_258d$i /" mt_inv.in
 	sed -i .bup "9 s/_270d.* /_270d$i /" mt_inv.in
-  echo $i
 	# RUN TDMT_INVC_ISO
   tdmt_invc_iso mt_inv.in
   # AWK TDMT_RESULT.OUT FOR z, VR, st1, rk1, dp1, st2, rk3, dp2
-  awk 'NR==2' tdmt_result.out | awk '{print $1,$3}' >> $outfile
-  awk 'NR==6' tdmt_result.out | awk '{print $1,$2,$3,$4,$5,$6}' >> $outfile
+  awk 'NR==2' tdmt_result.out | awk '{print -$1,$3}' >> $temp
+  awk 'NR==6' tdmt_result.out | awk '{print $1,$2,$3,$4,$5,$6}' >> $temp
 done
 
-#
+# MUNGE RESULTS TABLE
+awk "NR==1,EOF" $temp | sed "N;s/\n/ /" >> $outfile
+rm $temp
+
+# PLOT MT in GMT
+gmt psxy -R0/10/-25/0 -JX12/20 -Ba1f1g1/a2f1g2WSne -P -K -X3 -Y5 << END > $plot
+100 100
+END
+for i in {1..24}; do
+  awk "NR==$i" $outfile | awk '{print $2,$1,10,$3,$4,$5,$6,$7,$8,1,2,$2,$1}'
+  awk "NR==$i" $outfile | awk '{print $2,$1,10,$3,$4,$5,$6,$7,$8,1,2,$2,$1}' |\
+    gmt psmeca -R -J -Sc2 -K -O >> $plot
+done
+awk "NR==25" $outfile | gmt psmeca -R -J -Sc.1 -O >> $plot
